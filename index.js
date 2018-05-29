4a6f3754331d9315f92ebccf8e9565c8846c554b
Kuniaki Shimizu <kun432.8d1w@gmail.com>
2018-05-29 21:48:57 +0900

    add multi-language

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const Alexa = require('alexa-sdk');
 exports.handler = function (event, context, callback) {
   const alexa = Alexa.handler(event, context);
   alexa.appId = process.env.ALEXA_APPLICATION_ID;
+  alexa.resources = languageStrings;
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
 
+const languageStrings = {
+  'ja-JP': {
+    'translation': {
+      'HELLO': 'はじめまして',
+      'BYE': 'さようなら'
+    }
+  },
+  'en-US': {
+    'translation': {
+      'HELLO': 'Hello',
+      'BYE': 'bye'
+    }
+  }
+};
+
 var handlers = {
   'Unhandled': function () {
-    this.emit(':tell', 'はじめまして')
+    this.emit(':tell', this.t('HELLO'));
   }
-}
+};
